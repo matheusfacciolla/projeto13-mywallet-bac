d6b7b7776d58ad88d91b1235d73389ec71046058
pedro imbrech <elprogramadorpedro@gmail.com>
2022-05-19 16:44:20 -0300

    rev

--- routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signUp, signIn } from "../controllers/authControllers.js";
-import { validSignUp, validSignIn } from "../middlewares/authMiddlewares.js";
-
-const authRouter = Router();
-authRouter.post("/sign-up", validSignUp, signUp);
-authRouter.post("/", validSignIn, signIn);
-
-export default authRouter;
--- routes/transactionsRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { transactions, newTransaction } from "../controllers/transactionsControllers.js";
-import { validToken, validNewTransaction } from "../middlewares/transactionsMiddlewares.js";
-
-const transactionRouter = Router();
-transactionRouter.get("/transaction", validToken, transactions);
-transactionRouter.post("/transaction", validToken, validNewTransaction, newTransaction);
-
-export default transactionRouter;
